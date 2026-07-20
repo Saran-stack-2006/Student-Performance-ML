9114543a6c0610b94d3edb8356ffe869206ce8d9
SARAN G <sarangvss06@gmail.com>
2026-07-20 20:17:47 +0530

    Update settings.py

--- student/student/settings.py
+++ student/student/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-t*z@z0usxu4_-i+-pq$*#tovp1u@!f175n%z6aor=i@soxvvle"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
