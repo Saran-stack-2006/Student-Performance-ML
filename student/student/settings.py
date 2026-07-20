f6856c0b735aa41da922165ad825f7bb4ea1852c
SARAN G <sarangvss06@gmail.com>
2026-07-20 20:22:24 +0530

    Add storage configuration for static files

--- student/student/settings.py
+++ student/student/settings.py
@@ -128,4 +128,9 @@ STATIC_ROOT=os.path.join(BASE_DIR,"assets")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
