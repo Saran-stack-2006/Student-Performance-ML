b8a700df6db95a54b0d11f22997dbd438286cb1d
SARAN G <sarangvss06@gmail.com>
2026-07-20 20:53:59 +0530

    Update settings.py

--- student/student/settings.py
+++ student/student/settings.py
@@ -77,15 +77,10 @@ WSGI_APPLICATION = "student.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME":  "student",
-        "USER" : "root",
-        "PASSWORD" :"123456",
-        "HOST": "localhost",
-        "PORT" : "3308",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
